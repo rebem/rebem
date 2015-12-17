81357f1abae28ab9dbeec1294a491cb6b21fe160
deepsweet <kir@soulshine.in>
2015-12-17 10:52:28 +0700

    :heavy_minus_sign: remove "propTypes"

--- test/lib/chain.js
+++ test/lib/chain.js
@@ -2,222 +2,95 @@ import { expect } from 'chai';
 
 import Yummies from '../../lib/';
 import {
-    mergePropTypes,
     yummifyChain,
     yummifyChainRaw
 } from '../../lib/chain';
 
 describe('chain', () => {
     it('exist', () => {
-        expect(mergePropTypes).to.exist;
         expect(yummifyChain).to.exist;
         expect(yummifyChainRaw).to.exist;
     });
 
-    it('mergePropTypes()', () => {
-        expect(mergePropTypes([
+    it('yummifyChain()', () => {
+        let DummyClass1 = null;
+        let DummyClass2 = null;
+
+        const ResultFactory = yummifyChain([
             {
-                type: 'propTypes',
-                module: {
-                    prop1: 1,
-                    prop2: 2
+                type: 'main',
+                module(Base) {
+                    DummyClass1 = class extends Base {
+                        test1() {}
+                        render() {}
+                    };
+
+                    return DummyClass1;
                 }
             },
             {
-                type: 'propTypes',
-                module: {
-                    prop1: 3
+                type: 'main',
+                module(Base) {
+                    DummyClass2 = class extends Base {
+                        test2() {}
+                        render() {}
+                    };
+
+                    return DummyClass2;
                 }
+            },
+            {
+                type: 'styles',
+                module: true
             }
-        ])).to.be.deep.equal({
-            prop1: 3,
-            prop2: 2
-        });
+        ]);
+        const ResultClass = ResultFactory().type;
+        const ResultInstance = new ResultClass();
+
+        expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
+        expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
+        expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
+        expect(ResultInstance).to.have.property('test1');
+        expect(ResultInstance).to.have.property('test2');
     });
 
-    describe('yummifyChain()', () => {
-        it('with prop-types', () => {
-            let DummyClass1 = null;
-            let DummyClass2 = null;
+    it('yummifyChainRaw()', () => {
+        let DummyClass1 = null;
+        let DummyClass2 = null;
 
-            const ResultFactory = yummifyChain([
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass1 = class extends Base {
-                            test1() {}
-                            render() {}
-                        };
+        const ResultClassFactory = yummifyChainRaw([
+            {
+                type: 'main',
+                module(Base) {
+                    DummyClass1 = class extends Base {
+                        test1() {}
+                    };
 
-                        return DummyClass1;
-                    }
-                },
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass2 = class extends Base {
-                            test2() {}
-                            render() {}
-                        };
-
-                        return DummyClass2;
-                    }
-                },
-                {
-                    type: 'styles',
-                    module: true
-                },
-                {
-                    type: 'propTypes',
-                    module: {
-                        prop1: Yummies.PropTypes.number,
-                        prop2: Yummies.PropTypes.number
-                    }
+                    return DummyClass1;
                 }
-            ]);
-            const ResultClass = ResultFactory().type;
-            const ResultInstance = new ResultClass();
+            },
+            {
+                type: 'main',
+                module(Base) {
+                    DummyClass2 = class extends Base {
+                        test2() {}
+                    };
 
-            expect(ResultClass).to.have.property('propTypes');
-            expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
-            expect(ResultInstance).to.have.property('test1');
-            expect(ResultInstance).to.have.property('test2');
-        });
-
-        it('without prop-types', () => {
-            let DummyClass1 = null;
-            let DummyClass2 = null;
-
-            const ResultFactory = yummifyChain([
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass1 = class extends Base {
-                            test1() {}
-                            render() {}
-                        };
-
-                        return DummyClass1;
-                    }
-                },
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass2 = class extends Base {
-                            test2() {}
-                            render() {}
-                        };
-
-                        return DummyClass2;
-                    }
-                },
-                {
-                    type: 'styles',
-                    module: true
+                    return DummyClass2;
                 }
-            ]);
-            const ResultClass = ResultFactory().type;
-            const ResultInstance = new ResultClass();
+            },
+            {
+                type: 'styles',
+                module: true
+            }
+        ]);
+        const ResultClass = ResultClassFactory(Yummies.Component);
+        const ResultInstance = new ResultClass();
 
-            expect(ResultClass).to.not.have.property('propTypes');
-            expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
-            expect(ResultInstance).to.have.property('test1');
-            expect(ResultInstance).to.have.property('test2');
-        });
-    });
-
-    describe('yummifyChainRaw()', () => {
-        it('with prop-types', () => {
-            let DummyClass1 = null;
-            let DummyClass2 = null;
-
-            const ResultClassFactory = yummifyChainRaw([
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass1 = class extends Base {
-                            test1() {}
-                        };
-
-                        return DummyClass1;
-                    }
-                },
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass2 = class extends Base {
-                            test2() {}
-                        };
-
-                        return DummyClass2;
-                    }
-                },
-                {
-                    type: 'styles',
-                    module: true
-                },
-                {
-                    type: 'propTypes',
-                    module: {
-                        prop1: Yummies.PropTypes.number,
-                        prop2: Yummies.PropTypes.number
-                    }
-                }
-            ]);
-            const ResultClass = ResultClassFactory(Yummies.Component);
-            const ResultInstance = new ResultClass();
-
-            expect(ResultClass).to.have.property('propTypes');
-            expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
-            expect(ResultInstance).to.have.property('test1');
-            expect(ResultInstance).to.have.property('test2');
-        });
-
-        it('without prop-types', () => {
-            let DummyClass1 = null;
-            let DummyClass2 = null;
-
-            const ResultClassFactory = yummifyChainRaw([
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass1 = class extends Base {
-                            test1() {}
-                        };
-
-                        return DummyClass1;
-                    }
-                },
-                {
-                    type: 'main',
-                    module(Base) {
-                        DummyClass2 = class extends Base {
-                            test2() {}
-                        };
-
-                        return DummyClass2;
-                    }
-                },
-                {
-                    type: 'styles',
-                    module: true
-                }
-            ]);
-            const ResultClass = ResultClassFactory(Yummies.Component);
-            const ResultInstance = new ResultClass();
-
-            expect(ResultClass).to.not.have.property('propTypes');
-            expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
-            expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
-            expect(ResultInstance).to.have.property('test1');
-            expect(ResultInstance).to.have.property('test2');
-        });
+        expect(ResultInstance).to.be.an.instanceOf(Yummies.Component);
+        expect(ResultInstance).to.be.an.instanceOf(DummyClass1);
+        expect(ResultInstance).to.be.an.instanceOf(DummyClass2);
+        expect(ResultInstance).to.have.property('test1');
+        expect(ResultInstance).to.have.property('test2');
     });
 });
