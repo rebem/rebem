bab9c1200d4c3898616d9dcb9b7c8123bb6c7064
mistadikay <koltsov.d@gmail.com>
2016-02-22 11:49:09 +0700

    :wrench: move buildClassName and validate into separate package

--- test/lib/index.js
+++ test/lib/index.js
@@ -5,8 +5,7 @@ import assert from 'assert';
 
 import {
     BEM,
-    blockFactory,
-    buildClassName
+    blockFactory
 } from '../../lib/';
 
 function test(props, result) {
@@ -19,12 +18,6 @@ function test(props, result) {
 }
 
 describe('rebem', function() {
-    describe('buildClassName', function() {
-        it('is function', function() {
-            assert(typeof buildClassName === 'function');
-        });
-    });
-
     describe('BEM', function() {
         it('is function', function() {
             assert(typeof BEM === 'function');
@@ -55,276 +48,21 @@ describe('rebem', function() {
                 );
             });
 
-            it('props', function() {
+            it('buildClassName', function() {
                 test(
                     {
-                        className: 'test'
+                        block: 'block1',
+                        elem: 'elem',
+                        mods: {
+                            mod: 'val'
+                        },
+                        mix: {
+                            block: 'block2'
+                        },
+                        className: 'hello'
                     },
-                    '<div class="test"></div>'
+                    '<div class="block1__elem block1__elem_mod_val block2 hello"></div>'
                 );
-            });
-
-            describe('block', function() {
-                it('simple', function() {
-                    test(
-                        {
-                            block: 'block'
-                        },
-                        '<div class="block"></div>'
-                    );
-                });
-
-                it('props.className + block', function() {
-                    test(
-                        {
-                            block: 'block2',
-                            className: 'block1'
-                        },
-                        '<div class="block1 block2"></div>'
-                    );
-                });
-            });
-
-            describe('mods', function() {
-                describe('block', function() {
-                    it('block + mod', function() {
-                        test(
-                            {
-                                block: 'block',
-                                mods: {
-                                    mod: 'val'
-                                }
-                            },
-                            '<div class="block block_mod_val"></div>'
-                        );
-                    });
-
-                    it('block + few mods', function() {
-                        test(
-                            {
-                                block: 'block',
-                                mods: {
-                                    mod1: 'val1',
-                                    mod2: 'val2'
-                                }
-                            },
-                            '<div class="block block_mod1_val1 block_mod2_val2"></div>'
-                        );
-                    });
-
-                    it('block + shorthand mod = true', function() {
-                        test(
-                            {
-                                block: 'block',
-                                mods: {
-                                    mod: true
-                                }
-                            },
-                            '<div class="block block_mod"></div>'
-                        );
-                    });
-
-                    it('block + shorthand mod = false', function() {
-                        test(
-                            {
-                                block: 'block',
-                                mods: {
-                                    mod: false
-                                }
-                            },
-                            '<div class="block"></div>'
-                        );
-                    });
-                });
-
-                describe('elem', function() {
-                    it('block + elem + mod', function() {
-                        test(
-                            {
-                                block: 'block',
-                                elem: 'elem',
-                                mods: {
-                                    mod: 'val'
-                                }
-                            },
-                            '<div class="block__elem block__elem_mod_val"></div>'
-                        );
-                    });
-
-                    it('block + elem + few mods', function() {
-                        test(
-                            {
-                                block: 'block',
-                                elem: 'elem',
-                                mods: {
-                                    mod1: 'val1',
-                                    mod2: 'val2'
-                                }
-                            },
-                            '<div class="block__elem block__elem_mod1_val1 block__elem_mod2_val2"></div>'
-                        );
-                    });
-
-                    it('block + shorthand mod = true', function() {
-                        test(
-                            {
-                                block: 'block',
-                                elem: 'elem',
-                                mods: {
-                                    mod: true
-                                }
-                            },
-                            '<div class="block__elem block__elem_mod"></div>'
-                        );
-                    });
-
-                    it('block + shorthand mod = false', function() {
-                        test(
-                            {
-                                block: 'block',
-                                elem: 'elem',
-                                mods: {
-                                    mod: false
-                                }
-                            },
-                            '<div class="block__elem"></div>'
-                        );
-                    });
-                });
-            });
-
-            describe('mix', function() {
-                it('mix without block', function() {
-                    test(
-                        {
-                            mix: {
-                                block: 'block'
-                            }
-                        },
-                        '<div class="block"></div>'
-                    );
-                });
-
-                it('block + mix', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mix: {
-                                block: 'block2'
-                            }
-                        },
-                        '<div class="block1 block2"></div>'
-                    );
-                });
-
-                it('block + mods + mix', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mods: {
-                                mod: 'val'
-                            },
-                            mix: {
-                                block: 'block2'
-                            }
-                        },
-                        '<div class="block1 block1_mod_val block2"></div>'
-                    );
-                });
-
-                it('block + elem + mix', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            elem: 'elem',
-                            mix: {
-                                block: 'block2'
-                            }
-                        },
-                        '<div class="block1__elem block2"></div>'
-                    );
-                });
-
-                it('block + elem + mods + mix', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            elem: 'elem',
-                            mods: {
-                                mod: 'val'
-                            },
-                            mix: {
-                                block: 'block2'
-                            }
-                        },
-                        '<div class="block1__elem block1__elem_mod_val block2"></div>'
-                    );
-                });
-
-                it('complex mix', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mix: {
-                                block: 'block2',
-                                elem: 'elem',
-                                mods: {
-                                    mod1: 'val1',
-                                    mod2: 'val2'
-                                }
-                            }
-                        },
-                        '<div class="block1 block2__elem block2__elem_mod1_val1 block2__elem_mod2_val2"></div>'
-                    );
-                });
-
-                it('multiple mixes', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mix: [
-                                {
-                                    block: 'block2'
-                                },
-                                {
-                                    block: 'block3'
-                                }
-                            ]
-                        },
-                        '<div class="block1 block2 block3"></div>'
-                    );
-                });
-
-                it('multiple mixes with holes', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mix: [
-                                undefined,
-                                {
-                                    block: 'block2'
-                                },
-                                null
-                            ]
-                        },
-                        '<div class="block1 block2"></div>'
-                    );
-                });
-
-                it('recursive mixes', function() {
-                    test(
-                        {
-                            block: 'block1',
-                            mix: {
-                                block: 'block2',
-                                mix: {
-                                    block: 'block3'
-                                }
-                            }
-                        },
-                        '<div class="block1 block2 block3"></div>'
-                    );
-                });
             });
 
             it('bemjson is not mutated', function() {
